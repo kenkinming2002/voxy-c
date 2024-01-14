a21d279d86cb309cd3636bcd029146ea2aee2e23
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-01-15 02:14:35 +0800

    Spaces

--- resource_pack/src/resource_pack.c
+++ resource_pack/src/resource_pack.c
@@ -9,8 +9,8 @@ enum texture
 };
 
 const struct block_info block_infos[] = {
-  { .name = "grass", .texture_left = TEXTURE_GRASS_SIDE, .texture_right = TEXTURE_GRASS_SIDE, .texture_back = TEXTURE_GRASS_SIDE, .texture_front = TEXTURE_GRASS_SIDE, .texture_bottom = TEXTURE_GRASS_BOTTOM,  .texture_top = TEXTURE_GRASS_TOP },
-  { .name = "stone", .texture_left = TEXTURE_STONE,      .texture_right = TEXTURE_STONE,      .texture_back = TEXTURE_STONE,      .texture_front = TEXTURE_STONE,      .texture_bottom = TEXTURE_STONE,         .texture_top = TEXTURE_STONE     },
+  { .name = "grass", .texture_left = TEXTURE_GRASS_SIDE, .texture_right = TEXTURE_GRASS_SIDE, .texture_back = TEXTURE_GRASS_SIDE, .texture_front = TEXTURE_GRASS_SIDE, .texture_bottom = TEXTURE_GRASS_BOTTOM, .texture_top = TEXTURE_GRASS_TOP },
+  { .name = "stone", .texture_left = TEXTURE_STONE,      .texture_right = TEXTURE_STONE,      .texture_back = TEXTURE_STONE,      .texture_front = TEXTURE_STONE,      .texture_bottom = TEXTURE_STONE,        .texture_top = TEXTURE_STONE     },
 };
 
 const struct block_texture_info block_texture_infos[] = {

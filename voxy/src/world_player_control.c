cf771b21b53e45180a588ecc2464291c3e84ca10
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-01-30 20:49:08 +0800

    Fix: Pan speed should not be scaled by dt

--- voxy/src/world_player_control.c
+++ voxy/src/world_player_control.c
@@ -3,9 +3,9 @@
 void world_update_player_control(struct world *world, struct input *input, float dt)
 {
   static const float MOVE_SPEED = 50.0f;
-  static const float PAN_SPEED  = 0.2f;
+  static const float PAN_SPEED  = 0.002f;
 
-  fvec3_t rotation    = fvec3_mul_scalar(fvec3(input->mouse_motion.x, input->mouse_motion.y, 0.0f), PAN_SPEED * dt);
+  fvec3_t rotation    = fvec3_mul_scalar(fvec3(input->mouse_motion.x, input->mouse_motion.y, 0.0f), PAN_SPEED);
   fvec3_t translation = fvec3_mul_scalar(input->keyboard_motion, MOVE_SPEED * dt);
 
   transform_rotate(&world->player.transform, rotation);

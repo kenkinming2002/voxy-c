0ab4d8cf46bbe871909d6f0de921eb4df986e70f
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-02-24 04:19:03 +0800

    Documentation!?

--- voxy/include/voxy/main_game/chunk.h
+++ voxy/include/voxy/main_game/chunk.h
@@ -1,8 +1,8 @@
 #ifndef VOXY_MAIN_GAME_CHUNK_H
 #define VOXY_MAIN_GAME_CHUNK_H
 
-#include <voxy/main_game/block.h>
 #include <voxy/main_game/config.h>
+#include <voxy/main_game/block.h>
 #include <voxy/math/vector.h>
 
 #include <glad/glad.h>
@@ -12,36 +12,70 @@
 
 struct chunk
 {
-  struct chunk *next;
-  size_t        hash;
-  ivec3_t       position;
+  /*********
+   * Links *
+   *********/
+  struct
+  {
+    struct chunk *next;
+    size_t        hash;
+    ivec3_t       position;
 
-  struct chunk *bottom;
-  struct chunk *top;
-  struct chunk *back;
-  struct chunk *front;
-  struct chunk *left;
-  struct chunk *right;
+    struct chunk *bottom;
+    struct chunk *top;
+    struct chunk *back;
+    struct chunk *front;
+    struct chunk *left;
+    struct chunk *right;
+  };
 
-  bool          mesh_invalidated;
-  struct chunk *mesh_next;
+  /*****************
+   * Invalidations *
+   *****************/
+  struct
+  {
+    bool          mesh_invalidated;
+    struct chunk *mesh_next;
 
-  bool          light_invalidated;
-  struct chunk *light_next;
+    bool          light_invalidated;
+    struct chunk *light_next;
+  };
 
-  struct block blocks[CHUNK_WIDTH][CHUNK_WIDTH][CHUNK_WIDTH];
+  /********
+   * Data *
+   ********/
+  struct
+  {
+    struct block blocks[CHUNK_WIDTH][CHUNK_WIDTH][CHUNK_WIDTH];
+  };
 
-  bool culled;
+  /*************
+   * Rendering *
+   *************/
+  struct
+  {
+    bool culled;
 
-  GLuint vao_opaque;
-  GLuint vbo_opaque;
-  GLsizei count_opaque;
+    GLuint vao_opaque;
+    GLuint vbo_opaque;
+    GLsizei count_opaque;
 
-  GLuint vao_transparent;
-  GLuint vbo_transparent;
-  GLsizei count_transparent;
+    GLuint vao_transparent;
+    GLuint vbo_transparent;
+    GLsizei count_transparent;
+  };
 };
 
+/*
+ * Get pointer to block near a chunk, where position is in chunk local
+ * coordinate.
+ *
+ * When position.x, position.y, position.z are in the range [0, CHUNK_WIDTH),
+ * the returned block lie within supplied chunk. Otherwise, neighbour chunks
+ * will be traversed recursively. If chunk is NULL, or chunk becomes NULL during
+ * traversal, NULL is returned. This function is intended for used only when the
+ * queried block is close.
+ */
 struct block *chunk_get_block(struct chunk *chunk, ivec3_t position);
 
 #endif // VOXY_MAIN_GAME_CHUNK_H

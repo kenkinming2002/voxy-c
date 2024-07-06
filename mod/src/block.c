2d46677fd33624ae09cc07f2c4cd5755160504c2
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-07-06 19:39:13 +0800

    Fix block id to item id translation
    Fix block id to item id translation when spawning item entity after
    block breaking.

--- mod/src/block.c
+++ mod/src/block.c
@@ -14,11 +14,11 @@
 
 static item_id_t block_id_to_item_id(block_id_t block_id)
 {
-  if(block_id == BLOCK_ID_STONE) return BLOCK_ID_STONE;
-  if(block_id == BLOCK_ID_GRASS) return BLOCK_ID_GRASS;
-  if(block_id == BLOCK_ID_LOG)   return BLOCK_ID_LOG;
-  if(block_id == BLOCK_ID_LEAVE) return BLOCK_ID_LEAVE;
-  if(block_id == BLOCK_ID_LAMP)  return BLOCK_ID_LAMP;
+  if(block_id == BLOCK_ID_STONE) return ITEM_ID_STONE;
+  if(block_id == BLOCK_ID_GRASS) return ITEM_ID_GRASS;
+  if(block_id == BLOCK_ID_LOG)   return ITEM_ID_LOG;
+  if(block_id == BLOCK_ID_LEAVE) return ITEM_ID_LEAVE;
+  if(block_id == BLOCK_ID_LAMP)  return ITEM_ID_LAMP;
   assert(0 && "Unreachable");
 }
 

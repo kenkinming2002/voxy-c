5291a9cc772226a959bac7a858a1c1d250bc9262
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-07-15 01:34:56 +0800

    Remove unused defines in voxy/include/voxy/scene/main_game/config.h

--- mod/src/update/spawn_player.c
+++ mod/src/update/spawn_player.c
@@ -30,9 +30,9 @@ void update_spawn_player(void)
       // the chunk in which the player is to be spawned in need to be loaded
       // first.
       ivec3_t center = fvec3_as_ivec3_floor(fvec3_div_scalar(entity.position, CHUNK_WIDTH));
-      for(int dz = -GENERATOR_DISTANCE_PLAYER; dz<=GENERATOR_DISTANCE_PLAYER; ++dz)
-        for(int dy = -GENERATOR_DISTANCE_PLAYER; dy<=GENERATOR_DISTANCE_PLAYER; ++dy)
-          for(int dx = -GENERATOR_DISTANCE_PLAYER; dx<=GENERATOR_DISTANCE_PLAYER; ++dx)
+      for(int dz = -PLAYER_CHUNK_LOAD_DISTANCE; dz<=PLAYER_CHUNK_LOAD_DISTANCE; ++dz)
+        for(int dy = -PLAYER_CHUNK_LOAD_DISTANCE; dy<=PLAYER_CHUNK_LOAD_DISTANCE; ++dy)
+          for(int dx = -PLAYER_CHUNK_LOAD_DISTANCE; dx<=PLAYER_CHUNK_LOAD_DISTANCE; ++dx)
             enqueue_chunk_generate(ivec3_add(center, ivec3(dx, dy, dz)));
     }
     else
--- voxy/include/voxy/scene/main_game/config.h
+++ voxy/include/voxy/scene/main_game/config.h
@@ -9,35 +9,10 @@
 
 #define MOD_FILEPATH "mod/mod.so"
 
-#define UI_HOTBAR_COLOR_BACKGROUND fvec4(0.9f, 0.9f, 0.9f, 0.3f)
-#define UI_HOTBAR_COLOR_SELECTED   fvec4(0.95f, 0.75f, 0.75f, 0.8f)
-#define UI_HOTBAR_COLOR_HOVER      fvec4(0.4f,  0.8f,  0.2f,  0.8f)
-#define UI_HOTBAR_COLOR_DEFAULT    fvec4(0.95f, 0.95f, 0.95f, 0.7f)
-
-#define UI_INVENTORY_COLOR_BACKGROUND fvec4(0.9f, 0.9f, 0.9f, 1.0f)
-#define UI_INVENTORY_COLOR_HOVER      fvec4(0.4f,  0.8f,  0.2f,  0.8f)
-#define UI_INVENTORY_COLOR_DEFAULT    fvec4(0.95f, 0.95f, 0.95f, 0.7f)
-
-#define UI_TEXT_SIZE 28
-#define UI_TEXT_SIZE_ITEM_COUNT 20
-
-#define GENERATOR_DISTANCE_PLAYER 8
-
 #define RENDERER_LOAD_DISTANCE   4
 #define RENDERER_UNLOAD_DISTANCE 64
 
-#define PLAYER_MOVE_SPEED_GROUND 8.0f
-#define PLAYER_MOVE_SPEED_AIR    3.0f
-
-#define PLAYER_PAN_SPEED  0.002f
-#define PLAYER_JUMP_STRENGTH 8.0f
-#define PLAYER_ACTION_COOLDOWN 0.5f
-
-#define PLAYER_EYE_HEIGHT 0.9f
-#define PLAYER_DIMENSION fvec3(0.9f, 0.9f, 2.0f)
-
 #define PHYSICS_GRAVITY 9.8f
-
 #define PHYSICS_DRAG_GROUND 2.0f
 #define PHYSICS_DRAG_AIR    0.5f
 

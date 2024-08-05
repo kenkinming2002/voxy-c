a3412f476930a86c2c4fc295352dff656d1f1637
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-08-06 05:24:44 +0800

    Use render block tools to genearte item textures

---
+++ mod/bake.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./render_block/render_block -o mod/assets/textures/grass_item.png -w 512 -h 512 mod/assets/textures/grass_side.png mod/assets/textures/grass_side.png mod/assets/textures/grass_top.png
+./render_block/render_block -o mod/assets/textures/lamp_item.png -w 512 -h 512 mod/assets/textures/lamp.png mod/assets/textures/lamp.png mod/assets/textures/lamp.png
+./render_block/render_block -o mod/assets/textures/leave_item.png -w 512 -h 512 mod/assets/textures/leave.png mod/assets/textures/leave.png mod/assets/textures/leave.png
+./render_block/render_block -o mod/assets/textures/log_item.png -w 512 -h 512 mod/assets/textures/log_side.png mod/assets/textures/log_side.png mod/assets/textures/log_top_bottom.png
+./render_block/render_block -o mod/assets/textures/stone_item.png -w 512 -h 512 mod/assets/textures/stone.png mod/assets/textures/stone.png mod/assets/textures/stone.png

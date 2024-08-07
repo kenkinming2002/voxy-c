a5f9c0d5ab1a032d5c89938a595973502d93829b
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-08-07 23:00:12 +0800

    Prevent placing block over entities and opaque blocks

--- mod/src/item/item.c
+++ mod/src/item/item.c
@@ -1,6 +1,9 @@
 #include "item.h"
 
 #include <voxy/scene/main_game/states/chunks.h>
+#include <voxy/scene/main_game/states/entity_query.h>
+
+#include <libcommon/core/log.h>
 
 void item_on_use_place_block(struct entity *entity, struct item *item, block_id_t block_id)
 {
@@ -9,6 +12,19 @@ void item_on_use_place_block(struct entity *entity, struct item *item, block_id_
   if(entity_ray_cast(entity, 20.0f, &position, &normal))
     if(item->count > 0)
     {
+      const ivec3_t target_position = ivec3_add(position, normal);
+
+      const struct block_info *block_info = query_block_info(world_get_block_id(target_position));
+      if(block_info->type == BLOCK_TYPE_OPAQUE)
+        return;
+
+      struct entity **entities;
+      size_t entity_count;
+      world_query_entity(aabb3(ivec3_as_fvec3(target_position), fvec3(1.0f, 1.0f, 1.0f)), &entities, &entity_count);
+      free(entities);
+      if(entity_count != 0)
+        return;
+
       world_set_block(ivec3_add(position, normal), block_id, entity);
       if(--item->count == 0)
         item->id = ITEM_NONE;

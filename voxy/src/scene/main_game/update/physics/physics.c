2ea56ce3d6125ed87d0655e48e6ce5e4002f5c76
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-08-13 23:54:30 +0800

    Probably not the final fix to the physics system aka I hate floating point math.

--- voxy/src/scene/main_game/update/physics/physics.c
+++ voxy/src/scene/main_game/update/physics/physics.c
@@ -25,61 +25,69 @@ static void entity_physics_apply_law(struct entity *entity, float dt)
   entity->velocity = fvec3_mul_scalar(entity->velocity, expf(-dt * (entity->grounded ? PHYSICS_DRAG_GROUND : PHYSICS_DRAG_AIR))); // Drag
 }
 
+static int compar(const void *ptr1, const void *ptr2)
+{
+  const struct contact3 *contact1 = ptr1;
+  const struct contact3 *contact2 = ptr2;
+  return -contact3_compare(*contact1, *contact2);
+}
+
+DYNAMIC_ARRAY_DEFINE(contacts, struct contact3);
+
+static bool entity_physics_resolve_collision_once(struct entity *entity, float dt, struct contacts *contacts)
+{
+  const fvec3_t offset = fvec3_mul_scalar(entity->velocity, dt);
+
+  const struct entity_info *entity_info = query_entity_info(entity->id);
+  const aabb3_t entity_hitbox = aabb3(fvec3_add(entity->position, entity_info->hitbox_offset), entity_info->hitbox_dimension);
+  const aabb3_t entity_hitbox_expanded = aabb3_expand(entity_hitbox, offset);
+
+  const ivec3_t block_position_min = ivec3_sub_scalar(fvec3_as_ivec3_round(aabb3_min_corner(entity_hitbox_expanded)), 1);
+  const ivec3_t block_position_max = ivec3_add_scalar(fvec3_as_ivec3_round(aabb3_max_corner(entity_hitbox_expanded)), 1);
+  for(int z=block_position_min.z; z<=block_position_max.z; ++z)
+    for(int y=block_position_min.y; y<=block_position_max.y; ++y)
+      for(int x=block_position_min.x; x<=block_position_max.x; ++x)
+      {
+        const ivec3_t block_position = ivec3(x, y, z);
+        const block_id_t block_id = world_get_block_id(block_position);
+        if(block_id != BLOCK_NONE && query_block_info(block_id)->physics_type == BLOCK_PHYSICS_TYPE_CUBE)
+        {
+          const aabb3_t block_hitbox = aabb3(ivec3_as_fvec3(block_position), fvec3(1.0f, 1.0f, 1.0f));
+
+          struct contact3 contact1;
+          struct contact3 contact2;
+          if(swept_aabb3(entity_hitbox, block_hitbox, offset, &contact1, &contact2))
+            if(0.0f <= contact1.time && contact1.time < 1.0f)
+              DYNAMIC_ARRAY_APPEND(*contacts, contact1);
+        }
+      }
+
+  qsort(contacts->items, contacts->item_count, sizeof *contacts->items, compar);
+  for(size_t i=0; i<contacts->item_count; ++i)
+  {
+    const float nice = fvec3_dot(entity->velocity, contacts->items[i].normal);
+    if(nice < 0.0f)
+    {
+      // We need to increase the strength of our impulse a bit by multiplying it
+      // with 1.0001f. This so that we do not get accidentally stuck in a block
+      // due to floating point inaccurancy. Technically speaking, this small
+      // adjustment can be noticeable if our speed is high.
+      //
+      // FIXME: This is still incorrect.
+      const float strength = -nice * (1.0f - contacts->items[i].time) * 1.0001f;
+      const fvec3_t impulse = fvec3_mul_scalar(contacts->items[i].normal, strength);
+      entity->velocity = fvec3_add(entity->velocity, impulse);
+      return true;
+    }
+  }
+  return false;
+}
+
 static void entity_physics_resolve_collision(struct entity *entity, float dt)
 {
-  for(;;)
-  {
-    bool hit = false;
-    float min_t;
-    float max_s;
-    direction_t min_direction;
-
-    const fvec3_t offset = fvec3_mul_scalar(entity->velocity, dt);
-
-    const struct entity_info *entity_info = query_entity_info(entity->id);
-    const aabb3_t entity_hitbox = aabb3(fvec3_add(entity->position, entity_info->hitbox_offset), entity_info->hitbox_dimension);
-    const aabb3_t entity_hitbox_expanded = aabb3_expand(entity_hitbox, offset);
-
-    const ivec3_t block_position_min = fvec3_as_ivec3_round(aabb3_min_corner(entity_hitbox_expanded));
-    const ivec3_t block_position_max = fvec3_as_ivec3_round(aabb3_max_corner(entity_hitbox_expanded));
-    for(int z=block_position_min.z; z<=block_position_max.z; ++z)
-      for(int y=block_position_min.y; y<=block_position_max.y; ++y)
-        for(int x=block_position_min.x; x<=block_position_max.x; ++x)
-        {
-          const ivec3_t block_position = ivec3(x, y, z);
-          const block_id_t block_id = world_get_block_id(block_position);
-          if(block_id != BLOCK_NONE && query_block_info(block_id)->physics_type == BLOCK_PHYSICS_TYPE_CUBE)
-          {
-            const aabb3_t block_hitbox = aabb3(ivec3_as_fvec3(block_position), fvec3(1.0f, 1.0f, 1.0f));
-
-            float t1;
-            float t2;
-
-            float s1;
-            float s2;
-
-            direction_t direction1;
-            direction_t direction2;
-
-            if(swept_aabb3(entity_hitbox, block_hitbox, offset, &t1, &t2, &s1, &s2, &direction1, &direction2))
-              if((0.0f <= t1 && t1 < 1.0f) && (!hit || min_t > t1 || (min_t >= t1 && max_s < s1)))
-              {
-                hit = true;
-                min_t = t1;
-                max_s = s1;
-                min_direction = direction1;
-              }
-          }
-        }
-
-    // Update entity velocity accordingly if we hit something. We have to make
-    // sure that we are actually updating something and not get stuck in an
-    // infinite loop.
-    if(hit && entity->velocity.values[direction_axis(min_direction)] != 0.0f)
-      entity->velocity.values[direction_axis(min_direction)] *= min_t;
-    else
-      break;
-  }
+  struct contacts contacts = {0};
+  while(entity_physics_resolve_collision_once(entity, dt, &contacts))
+    contacts.item_count = 0;
 }
 
 static void entity_physics_integrate(struct entity *entity, float dt)
--- voxy/src/scene/main_game/update/physics/swept.c
+++ voxy/src/scene/main_game/update/physics/swept.c
@@ -1,145 +1,96 @@
 #include <voxy/scene/main_game/update/physics/swept.h>
 
-/// Solve for the interval (*t1, t2) such that aabb1 intersect with aabb2 if aabb1
-/// is translated by t * offset where t is in (*t1, *t2).
-bool swept_aabb1(aabb1_t aabb1, aabb1_t aabb2, fvec1_t offset, float *t1, float *t2, bool *direction1, bool *direction2)
+#include <libcommon/utils/utils.h>
+#include <libcommon/core/log.h>
+
+int contact3_compare(struct contact3 contact1, struct contact3 contact2)
 {
-  const float a1 = aabb1_min_corner(aabb1).x;
-  const float b1 = aabb1_max_corner(aabb1).x;
+  if(contact1.time < contact2.time) return 1;
+  if(contact1.time > contact2.time) return -1;
 
-  const float a2 = aabb1_min_corner(aabb2).x;
-  const float b2 = aabb1_max_corner(aabb2).x;
+  if(contact1.area < contact2.area) return -1;
+  if(contact1.area > contact2.area) return 1;
 
-  const float v = offset.x;
-
-  *direction1 = signbit(v);
-  *direction2 = !signbit(v);
-
-  if(v != 0.0f)
-  {
-    *t1 = (a2 - b1) / v;
-    *t2 = (b2 - a1) / v;
-    if(*t1 > *t2)
-    {
-      const float tmp = *t1;
-      *t1 = *t2;
-      *t2 = tmp;
-    }
-    return *t1 < *t2;
-  }
-  else
-  {
-    *t1 = -INFINITY;
-    *t2 = +INFINITY;
-    return b1 > a2 && b2 > a1;
-  }
+  return 0;
 }
 
-/// Compute contact area between aabb1 and aabb2, assuming they are touching
-/// along omit_axis.
-static float contact_surface(aabb3_t aabb1, aabb3_t aabb2, unsigned omit_axis)
+bool swept_aabb3(aabb3_t aabb1, aabb3_t aabb2, fvec3_t offset, struct contact3 *contact1, struct contact3 *contact2)
 {
-  const fvec3_t as1 = aabb3_min_corner(aabb1);
-  const fvec3_t bs1 = aabb3_max_corner(aabb1);
+  const fvec3_t min_corner1 = aabb3_min_corner(aabb1);
+  const fvec3_t min_corner2 = aabb3_min_corner(aabb2);
 
-  const fvec3_t as2 = aabb3_min_corner(aabb2);
-  const fvec3_t bs2 = aabb3_max_corner(aabb2);
+  const fvec3_t max_corner1 = aabb3_max_corner(aabb1);
+  const fvec3_t max_corner2 = aabb3_max_corner(aabb2);
 
-  float result = 1.0f;
+  bool initialized = false;
+
+  unsigned axis1;
+  unsigned axis2;
+
+  // Check for collision times and normals along all axis.
   for(unsigned axis=0; axis<3; ++axis)
-    if(axis != omit_axis)
+    if(offset.values[axis] != 0.0f)
     {
-      const float a1 = as1.values[axis];
-      const float b1 = bs1.values[axis];
+      const float t1 = (min_corner2.values[axis] - max_corner1.values[axis]) / offset.values[axis];
+      const float t2 = (max_corner2.values[axis] - min_corner1.values[axis]) / offset.values[axis];
 
-      const float a2 = as2.values[axis];
-      const float b2 = bs2.values[axis];
+      if(!initialized || contact1->time < MIN(t1, t2))
+      {
+        axis1 = axis;
+        contact1->time = MIN(t1, t2);
+        contact1->normal = direction_as_fvec(direction_from_sign_axis(offset.values[axis] < 0.0f, axis));
+      }
 
-      const float a = a1 > a2 ? a1 : a2;
-      const float b = b1 < b2 ? b1 : b2;
+      if(!initialized || contact2->time > MAX(t1, t2))
+      {
+        axis2 = axis;
+        contact2->time = MAX(t1, t2);
+        contact2->normal = direction_as_fvec(direction_from_sign_axis(offset.values[axis] > 0.0f, axis));
+      }
 
-      result *= b - a;
+      initialized = true;
     }
+    else if(max_corner1.values[axis] <= min_corner2.values[axis] || max_corner2.values[axis] <= min_corner1.values[axis])
+        return false;
 
-  return result;
-}
-
-/// Solve for the interval (*t1, t2) such that aabb1 intersect with aabb2 if
-/// aabb1 is translated by t * offset where t is in (*t1, *t2).
-///
-/// Unforuntately, we run into the "t-fighting" problem if we hit two aabbs at
-/// the same time. For example:
-///
-///           -----------
-///           |         |
-///           |         |
-///           |         |
-///           |    E    |
-///           |         |
-///           |         |
-///     ------*----------
-///     |     |     |
-///     |  A  |  B  |
-///     |     |     |
-///     -------------
-///
-/// Entity E will hit both A and B at t = 0.0. To disambiguate, we also compute
-/// *s1 and *s2 which are the contact surface area at *t1 and *t2 respectively.
-/// In the above case, the contact with A is a line and hence the area is zero,
-/// whereas the contact area with B is a plane and hence the area is non-zero.
-/// We prioritize collision with largest contact area.
-bool swept_aabb3(aabb3_t aabb1, aabb3_t aabb2, fvec3_t offset, float *t1, float *t2, float *s1, float *s2, direction_t *direction1, direction_t *direction2)
-{
-  float ts1[3];
-  float ts2[3];
-
-  bool directions1[3];
-  bool directions2[3];
-
-  for(unsigned axis=0; axis<3; ++axis)
-  {
-    const aabb1_t sub_aabb1 = { .center = fvec1(aabb1.center.values[axis]), .dimension = fvec1(aabb1.dimension.values[axis]) };
-    const aabb1_t sub_aabb2 = { .center = fvec1(aabb2.center.values[axis]), .dimension = fvec1(aabb2.dimension.values[axis]) };
-    const fvec1_t sub_offset = fvec1(offset.values[axis]);
-    if(!swept_aabb1(sub_aabb1, sub_aabb2, sub_offset, &ts1[axis], &ts2[axis], &directions1[axis], &directions2[axis]))
-      return false;
-  }
-
-  *t1 = -INFINITY;
-  *t2 = +INFINITY;
-  for(unsigned axis=0; axis<3; ++axis)
-  {
-    if(*t1 < ts1[axis])
-    {
-      *t1 = ts1[axis];
-      *direction1 = direction_from_sign_axis(directions1[axis], axis);
-    }
-
-    if(*t2 > ts2[axis])
-    {
-      *t2 = ts2[axis];
-      *direction2 = direction_from_sign_axis(directions2[axis], axis);
-    }
-  }
-
-  if(*t1 >= *t2)
+  // There only reason we are not initialized is if
+  //  - We are intersecting on all axis.
+  //  - Offset is zero so we never "un-intersect" ourselves.
+  // Bails out.
+  if(!initialized)
     return false;
 
-  const fvec3_t saved_center = aabb1.center;
+  if(contact1->time >= contact2->time)
+    return false;
 
-  aabb1.center = fvec3_add(aabb1.center, fvec3_mul_scalar(offset, *t1));
+  // Now comes the important part. We want to compute the contact surface area
+  // at time of collision.
+  contact1->area = 1.0f;
+  contact2->area = 1.0f;
+  for(unsigned axis = 0; axis < 3; ++axis)
   {
-    *s1 = 1.0f;
-    *s1 = contact_surface(aabb1, aabb2, direction_axis(*direction1));
-  }
-  aabb1.center = saved_center;
+    if(axis != axis1)
+    {
+      const float a1 = min_corner1.values[axis] + contact1->time * offset.values[axis];
+      const float b1 = max_corner1.values[axis] + contact1->time * offset.values[axis];
 
-  aabb1.center = fvec3_add(aabb1.center, fvec3_mul_scalar(offset, *t2));
-  {
-    *s2 = 1.0f;
-    *s2 = contact_surface(aabb1, aabb2, direction_axis(*direction2));
+      const float a2 = min_corner2.values[axis];
+      const float b2 = max_corner2.values[axis];
+
+      contact1->area *= MAX(MIN(a1, b1), MIN(a2, b2)) - MIN(MAX(a1, b1), MAX(a2, b2));
+    }
+
+    if(axis != axis2)
+    {
+      const float a1 = min_corner1.values[axis] + contact2->time * offset.values[axis];
+      const float b1 = max_corner1.values[axis] + contact2->time * offset.values[axis];
+
+      const float a2 = min_corner2.values[axis];
+      const float b2 = max_corner2.values[axis];
+
+      contact2->area *= MAX(MIN(a1, b1), MIN(a2, b2)) - MIN(MAX(a1, b1), MAX(a2, b2));
+    }
   }
-  aabb1.center = saved_center;
 
   return true;
 }

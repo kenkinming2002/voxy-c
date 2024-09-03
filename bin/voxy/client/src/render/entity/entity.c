c84d7e45bde99cadd23e08c567be17f55a24488e
Kwok Kin Ming <kenkinming2002@gmail.com>
2024-09-03 18:38:05 +0800

    Skip rendering of dead entity

--- bin/voxy/client/src/render/entity/entity.c
+++ bin/voxy/client/src/render/entity/entity.c
@@ -7,9 +7,12 @@ void entity_renderer_render(struct entity_registry *entity_registry, struct enti
   for(size_t i=0; i<entity_manager->entities.item_count; ++i)
   {
     const struct entity *entity = &entity_manager->entities.items[i];
-    const struct entity_info *info = &entity_registry->infos.items[entity->id]; // FIXME: Potential out of bounds.
-    if(info->render)
-      info->render(entity, camera);
+    if(entity->alive)
+    {
+      const struct entity_info *info = &entity_registry->infos.items[entity->id]; // FIXME: Potential out of bounds.
+      if(info->render)
+        info->render(entity, camera);
+    }
   }
 }
 
